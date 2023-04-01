bd11691b7244a65d8c1ac6fae295f4b9bd9b1105
mefju2 <55331075+mefju2@users.noreply.github.com>
2023-04-01 17:59:41 +0200

    Clean up and adding a parameter - frames_to_avoid to the extract_images_vertical.

--- tabs_main.py
+++ tabs_main.py
@@ -4,66 +4,31 @@ import pytesseract
 from settings import Settings
 
 
-def extractImagesHorizontal(pathIn, pathOut):
-    count = 14
-    vidcap = cv2.VideoCapture(pathIn)
-    success, image = vidcap.read()
-    success = True
-    blank_image = np.zeros((1920, 1280, 3), np.uint8)
-    page_height = 320
-    current_page = [None, None, None, None, None, None]
-    page_part_counter = 0
-
-    while success:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*950))
-        success,image = vidcap.read()
-        print('Read a new frame: ', success)
-
-        if image is not None:
-            cropped_img = image[400:1080, 0:1920]
-
-            a4 = blank_image
-            a4[page_height - 320:page_height, 0:1280] = cropped_img
-
-            #cv2.imwrite(f"{pathOut}/frame_{count}.jpg", cropped_img)
-            current_page[page_part_counter] = a4
-
-            if page_height == 1920:
-                cv2.imwrite(f"{pathOut}/a4_full_{count}.jpg", a4)
-                page_height = 320
-                blank_image[:, :] = (255, 255, 255)
-                page_part_counter = 0
-                current_page = [None, None, None, None, None, None]
-            else:
-                #cv2.imwrite(f"{pathOut}/a4_{count}.jpg", a4)
-                page_height += 320
-
-            count = count + 14
-    cv2.imwrite(f"{pathOut}/a4_full_{count}.jpg", current_page[page_part_counter])
-
-
-def extract_images_vertical(path_in, path_out, increment_by, settings):
+def extract_images_vertical(path_in, path_out, increment_by, settings, frames_to_avoid=()):
     vidcap = cv2.VideoCapture(path_in)
-    success, image = vidcap.read()
+    vidcap.read()
     success = True
 
     page_height = settings.crop_height_cords[1] - settings.crop_height_cords[0]
     current_page_height = page_height
     page_width = 0
 
-    blank_image = np.zeros((page_height * 5, 2560, 3), np.uint8)  # 320 * 5 / 1280 * 2
+    blank_image = np.zeros((page_height * 5, 2560, 3), np.uint8)  # for Sky Guitar videos it's 320 * 5 / 1280 * 2
     blank_image[:, :] = (255, 255, 255)
 
     current_page = [None, None, None, None, None]
     page_part_counter = 0
-    previous_measure_number = 5
     previous_measure = None
     count = increment_by
 
     while success:
+        if len(frames_to_avoid) > 0:
+            if count in frames_to_avoid:
+                count += increment_by
+                continue
+
         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*950))
         success, image = vidcap.read()
-        # print('Read a new frame: ', success)
 
         if image is not None:  # image is 720x1280
             cropped_img = image[
@@ -71,14 +36,6 @@ def extract_images_vertical(path_in, path_out, increment_by, settings):
                           0:1280]
 
             current_measure = get_measures_section(image, settings)
-            # measure_number = get_measure_number(image, previous_measure_number, pathOut, settings)
-            # if measure_number == 56:
-            #     cv2.imwrite(f"{pathOut}/measure_{measure_number}_{count}.jpg", cropped_img)
-            # print(measure_number)
-
-            # difference_between_measures = abs(measure_number - previous_measure_number)
-            # print(f"|{measure_number} - {previous_measure_number}| = {difference_between_measures}")
-            # if measure_number != previous_measure_number and 3 < difference_between_measures < 7:
             if previous_measure is None or not measures_are_equal(current_measure, previous_measure, path_out, count):
                 # print(count)
                 a4 = blank_image
@@ -106,15 +63,14 @@ def extract_images_vertical(path_in, path_out, increment_by, settings):
 
             count = count + increment_by
             previous_measure = current_measure
-    if current_page[0] is not None:  # po to zapisać kartkę, gdy ta nie jest zapełniona do końca
+    if current_page[0] is not None:  # printing the last page if it's not filled up
         cv2.imwrite(f"{path_out}/a4_full_{count}.jpg", current_page[page_part_counter])
 
 
 def get_measures_section(frame, settings):
     return frame[
                   settings.measure_number_height_cords[0]:settings.measure_number_height_cords[1],
-                  settings.measure_number_width_cords[0]:settings.measure_number_width_cords[1]
-                  ]
+                  settings.measure_number_width_cords[0]:settings.measure_number_width_cords[1]]
 
 
 def measures_are_equal(current_measure, previous_measure, path_out, count):
@@ -127,11 +83,9 @@ def measures_are_equal(current_measure, previous_measure, path_out, count):
             if (f - pf).any():
                 different += 1
 
-    # print(f'{str(len(current_measure) * len(current_measure[0] * 3))} - {different}')
-
     if different > 200:
-        c_ocr = read_measure_with_ocr(current_measure, count)
-        p_ocr = read_measure_with_ocr(previous_measure, count)
+        c_ocr = read_measure_with_ocr(current_measure)
+        p_ocr = read_measure_with_ocr(previous_measure)
         # if 100 > count > 50:
         #     cv2.imwrite(f"{path_out}/{count}_{different}_{c_ocr}_current_measure.jpg", current_measure)
         #     cv2.imwrite(f"{path_out}/{count}_{different}_{p_ocr}_previous_measure.jpg", previous_measure)
@@ -143,10 +97,10 @@ def measures_are_equal(current_measure, previous_measure, path_out, count):
     return True
 
 
-def read_measure_with_ocr(measure, count):
+def read_measure_with_ocr(measure):
     read_number = pytesseract.image_to_string(measure, config='--psm 6').strip()
 
-    # obsługa wyjątków gdy ocr źle przeczyta
+    # handling weird ocr reads
     if read_number == 'oL':
         return '51'
     if '.' in read_number:
@@ -159,10 +113,9 @@ def run():
     guitar_settings = Settings((400, 720), (430, 460), (45, 80))
     piano_settings = Settings((0, 255), (0, 25), (110, 150))
     out_path = "frames"
-    # extractImagesHorizontal(in_path, out_path)
-    # extractImagesVertical("The Entertainer.mp4", out_path, 14)  # The Entertainer
-    extract_images_vertical("Married_Life.mp4", out_path, 10, guitar_settings)  # Married Life
-    #extract_images_vertical("SPRING DAY, CHERRY BLOSSOMS.mp4", out_path, 4, piano_settings)  # Married Life
+    # extractImagesVertical("The Entertainer.mp4", out_path, 14)
+    # extract_images_vertical("Married_Life.mp4", out_path, 10, guitar_settings, (230,))
+    extract_images_vertical("SPRING DAY, CHERRY BLOSSOMS.mp4", out_path, 4, piano_settings)
 
 
 run()
